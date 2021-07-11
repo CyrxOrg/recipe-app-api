cc21b7556b5e493a6925a2c966157c6b8e716edb
CyrxOrg <daniel@engleitner-oeg.at>
2021-07-11 12:14:25 +0200

    create user and create superuser

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,4 +13,28 @@ class ModelTests(TestCase):
     )
 
     self.assertEqual(user.email, email)
-    self.assertTrue(user.check_Password(password))
+    self.assertTrue(user.check_password(password))
+
+  def test_new_user_email_normalized(self):
+    """Test the email for a new user is normalized"""
+    email = "test@CYRX.org"
+    user = get_user_model().objects.create_user(email, 'notUsedPassword')
+
+    self.assertEqual(user.email, email.lower())
+
+
+  def test_new_user_invalid_email(self):
+    """Test creating user with no email raises error"""
+    with self.assertRaises(ValueError):
+      get_user_model().objects.create_user(None, 'notUsedPassword')
+
+  def test_create_new_superuser(self):
+    """Test superuser creation"""
+    user = get_user_model().objects.create_superuser(
+      'test@cyrx.org',
+      'NotUsedPassword'
+    )
+
+    self.assertTrue(user.is_superuser)
+    self.assertTrue(user.is_staff)
+    
